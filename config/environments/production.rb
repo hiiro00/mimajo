ec828aa1f46b0f092882890e39580e8d48bd6de4
hiiro00 <totugekida@gmail.com>
2020-05-10 12:42:25 +0000

    15th action cable production kousin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,13 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier  heroku taiou
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
+  config.assets.initialize_on_precompile = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile += ['cable.js']
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
