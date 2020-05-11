bbccfa6e6e460c644d668a451ae5069f5f1aaa90
hiiro00 <totugekida@gmail.com>
2020-05-11 14:10:52 +0000

    15th action cable production kousin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile += ['cable.js']
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
